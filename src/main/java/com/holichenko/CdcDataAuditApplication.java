814f285b34049c1b02c351cf5df189af16de57dd
Volodymyr Holichenko <vladimirgolicenko154@gmail.com>
2024-09-27 21:45:36 +0300

    init project

---
+++ src/main/java/com/holichenko/CdcDataAuditApplication.java
@@ -0,0 +1,13 @@
+package com.holichenko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdcDataAuditApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CdcDataAuditApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/holichenko/CdcDataAuditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.holichenko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdcDataAuditApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
